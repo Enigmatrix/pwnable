1a2cf0a65cc8d3d88d407d0f1960328768a43dd4
Enigmatrix <enigmatrix2000@gmail.com>
2018-12-23 15:53:27 +0800

    add chall thingy

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,15 @@
 import express, {Application} from 'express';
+import challs from './routes/chall';
 
 export default (app:Application) => {
-  // app.use(express.json());
-  //
-  app.get('/foo2', (req, res) => {
+  app.use(express.json());
+  
+  app.get('/foo', (req, res) => {
      res.json({msg: 'foo'});
   });
+
+  app.use('/chall', challs);
+
   //
   // app.post('/bar', (req, res) => {
   //   res.json(req.body);
---
+++ server/routes/chall.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+let challs = express.Router();
+
+challs.post('/create', (req, res) => {
+
+});
+
+
+export default challs;
