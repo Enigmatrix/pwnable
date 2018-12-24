f1f8e0048886a46d65c4177f5aac2b87f8b86660
Enigmatrix <enigmatrix2000@gmail.com>
2018-12-24 18:21:12 +0800

    rmv shite

--- server/routes/chall.ts
+++ server/routes/chall.ts
@@ -7,8 +7,6 @@ import { Container } from 'dockerode';
 import {GDB} from 'gdb-js';
 import MemoryStream from 'memorystream';
 
-
-
 let challs = express.Router();
 
 let getChall = async id => {
@@ -21,44 +19,6 @@ let getChall = async id => {
 let getChallStream = async (chall:Container) => {
     return await chall.attach({
         stream: true, stdin: true, stdout: true, stderr: true});
-};
-
-let tillGdbCmdSep = (stream: NodeJS.ReadWriteStream, cmd):Promise<string[]> => {
-    return new Promise(res => {
-        let result = Buffer.from("");
-        const needle = '(gdb) ';
-        let listener = (dat: Buffer|string) => {
-            if(!(dat instanceof Buffer))
-                dat = Buffer.from(dat);
-            let sep = dat.indexOf(needle);
-            //console.log('BUFF>>', dat.toString());
-            if(sep >= 0){
-                result = Buffer.concat([result, dat.slice(0, sep)]);
-                stream.unshift(dat.slice(sep+needle.length));
-                stream.removeListener('data', listener);
-                //console.log(result.toString())
-                let out = result.toString().split('\n')
-                    .filter(x => x.indexOf("~\"") >= 0 && x.indexOf("\\n\"") >= 0)
-                    .map(x => beforeLast(afterFirst(x, "~\""), "\\n\""))
-                    .map(x => JSON.parse('"'+x+'"'));
-                res(out);
-            } else {
-                result = Buffer.concat([result, dat]);
-            }
-        }
-        stream.on('data', listener);
-        stream.write(cmd);
-    });
-}
-
-let gdbCmdInit = async (chall: Container) => {
-    let stream = await getChallStream(chall);
-    await tillGdbCmdSep(stream, '');
-};
-
-let gdbCmd = async (chall:Container, cmd):Promise<string[]> => {
-    let stream = await getChallStream(chall);
-    return await tillGdbCmdSep(stream, cmd+'\n');
 };
 
 challs.post('/new', async (req, res) => {
@@ -95,13 +55,6 @@ challs.post('/new', async (req, res) => {
     console.log(await gdb.execCLI('file bof1'));
     let asmsrc = await gdb.execCLI('x/10i main');
     let csrc = await gdb.execCLI('list main');
-
-    /*await gdbCmdInit(chall);
-    await gdbCmd(chall, 'cd ./bof1');
-    await gdbCmd(chall, 'file ./bof1');
-    await gdbCmd(chall, 'set disassembly-flavor intel');
-    let asmsrc = await gdbCmd(chall, 'x/10i main');
-    let csrc = await gdbCmd(chall, 'list main');*/
     
     res.json({
         id: chall.id,
